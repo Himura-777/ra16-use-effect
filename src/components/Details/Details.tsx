e7f2cc8934b64262678e56a8af0b9cab1d1234d0
Tuan Anh <tp.anhh7@gmail.com>
2025-07-20 11:47:33 +0300

    Initial commit

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useFetch } from "../../hooks/useFetch";
 import Loader from "../Loader/Loader";
 import { type UserDetails } from "./Details.types";
@@ -6,11 +6,16 @@ import { type UserDetails } from "./Details.types";
 const Details: React.FC<{
 	info: { id: number | null; name: string } | null;
 }> = ({ info }) => {
+	const [avatarVersion, setAvatarVersion] = useState(0);
 	const url = info?.id
 		? `https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${info.id}.json`
 		: "";
 
 	const { data, loading, error } = useFetch<UserDetails>(url);
+
+	useEffect(() => {
+		setAvatarVersion(prev => prev + 1);
+	}, [info?.id]);
 
 	if (!info) return <div className='details'>Select a user</div>;
 
@@ -21,7 +26,11 @@ const Details: React.FC<{
 			{error && <div className='error'>{error}</div>}
 			{data && (
 				<div className='user-details'>
-					<img src={data.avatar} alt={data.name} />
+					<img
+						src={`${data.avatar}?v=${avatarVersion}`}
+						alt={data.name}
+						key={`avatar-${avatarVersion}`}
+					/>
 					<p>City: {data.details.city}</p>
 					<p>Company: {data.details.company}</p>
 					<p>Position: {data.details.position}</p>
